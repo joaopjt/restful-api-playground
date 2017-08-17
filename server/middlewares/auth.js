180256e2ee105df5bd22bb2c72402a795475f521
João Julio <joao.tavares@novahaus.com.br>
2017-08-17 19:53:34 -0300

    Added auth middleware with jsonWebToken

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,29 @@
-// Auth middleware with JWT
-const jwt = 'REQUIRE JWT HERE';
+// Auth middleware with JSON Web Token
+const jwt = require('jsonwebtoken');
+const Boom = require('boom');
 
 module.exports = function (Server) {
-  if (!App.isProd) console.log('AUTH MIDDLEWARE NEED TO BE ADDED BEFORE LOAD.')
+  let unauthorized = Bomm.unauthorized('Token invalid or expired!');
+
+  const auth = function () {
+    return {
+      authenticate: function (request, res) {
+        const token = request.headers.authorization;
+        
+        if (!token) {
+          return res(unauthorized).code(401);
+        } else {
+          let decoded = jwt.verify(token, App.JSON_PASS);
+          
+          if(!decoded) return res(unauthorized).code(401);
+
+          res.continue({ credentials: decoded });
+        }   
+      }
+    }
+  };
+
+  //Register validation schema
+  Server.auth.scheme('jwt', auth);
+  Server.auth.strategy('default', 'jwt');
 }
